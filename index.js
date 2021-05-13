2165cc52a981a79e1402f57ae502d71779505307
Alice Genkin <agenkin1@gmail.com>
2021-05-12 19:28:16 -0700

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet (string) {
+  string = string.split(":")
+  if (string[0] < 12) {
+    return "Good Morning"
+  } 
+  else if (string[0] < 17) {
+    return "Good Afternoon"
+  } 
+  else return "Good Evening"
+
+}
+
+function displayMessage(msg) {
+  document.getElementById("greeting").innerText = msg
+}
